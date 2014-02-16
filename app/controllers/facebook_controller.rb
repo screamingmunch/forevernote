132e7e8b939396005079d43690a2099ee595bea6
Bryan K <bkahler3@gmail.com>
2014-02-16 11:33:46 -0800

    facebook is working

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -4,7 +4,14 @@ class FacebookController < ApplicationController
     fb_user = User.find_by_id(current_user)
     facebook_token = fb_user.linkedids.find_by_provider("facebook").token
     @graph = Koala::Facebook::API.new(facebook_token)
-    binding.pry
+    @feed = @graph.get_connection('me','feed')
+
+    @fb_messages = []
+    @feed.each do |message|
+      if message['message']
+        @fb_messages.push(message['message'])
+      end
+    end
   end
 
 end
