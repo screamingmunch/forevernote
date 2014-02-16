3aac5ffa96b2c1574d2f31abca9b388c034b4f2b
Bryan K <bkahler3@gmail.com>
2014-02-15 17:54:05 -0800

    added facebook access

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Forevernote::Application.routes.draw do
   match "/auth/twitter/callback" => "sessions#create"
   match "/auth/instagram/callback" => "sessions#access"
   match "/auth/evernote/callback" => "sessions#access"
+  match "/auth/facebook/callback" => "sessions#access"
 
   match "/signout" => "sessions#destroy", :as => :signout
 
