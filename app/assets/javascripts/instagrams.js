093ca6976e70a126cc2277a4c0dd3ebc8cc49c1c
Kristine <screamingmunch@gmail.com>
2014-02-16 14:10:16 -0800

    instagram more

--- app/assets/javascripts/instagrams.js
+++ app/assets/javascripts/instagrams.js
@@ -1,3 +1,44 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(function(){
+  $('body').on('click', '#instagram', function(event){
+    event.preventDefault();
+    console.log('clicked instagram');
+  $.get('/instagram.json').done(function(data) {
+
+  // _.each(data, function(item) {
+
+    // Create marker
+    //var itemData = new google.maps.LatLng( parseFloat(item.latitude), parseFloat(item.longitude) )
+
+    // Add pothole content
+    // var potholeContent = JST['templates/pothole'](item);
+
+    // Execute
+    // createMarker(itemData, potholeContent, item.id);
+
+    var image1 = data[0].images.standard_resolution.url;
+    var image2 = data[1].images.standard_resolution.url;
+    var image3 = data[2].images.standard_resolution.url;
+    var image4 = data[3].images.standard_resolution.url;
+    var image5 = data[4].images.standard_resolution.url;
+    var image6 = data[5].images.standard_resolution.url;
+    var image7 = data[6].images.standard_resolution.url;
+    var image8 = data[7].images.standard_resolution.url;
+    var image9 = data[8].images.standard_resolution.url;
+    var image10 = data[9].images.standard_resolution.url;
+
+    console.log(image2);
+    $('.instagram').append("test");
+
+
+
+
+
+  })
+
+
+
+
+
+  })
+
+})
